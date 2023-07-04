250fe25e85c0f25edac24e1da58683a64526420c
Davi Nunes <davinunes.franca@eb.mil.br>
2023-07-04 14:55:29 -0300

    Commit Inicial

---
+++ src/main/java/br/eti/davinunes/api/parkingcontrol/ParkingControlApplication.java
@@ -0,0 +1,13 @@
+package br.eti.davinunes.api.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+}
